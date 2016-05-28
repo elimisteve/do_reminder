e2d45a4330a7f6af3bbbc19c6d5974ea5604fe95
Steve Phillips <steve@tryingtobeawesome.com>
2016-05-28 01:42:13 -0700

    text command now uses twilhelp.SendSMS

--- text/text.go
+++ text/text.go
@@ -4,20 +4,11 @@
 package main
 
 import (
-	"encoding/xml"
 	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/subosito/twilio"
-)
-
-var (
-	twilioAccount = os.Getenv("TWILIO_ACCOUNT")
-	twilioKey     = os.Getenv("TWILIO_KEY")
-	myNumber      = os.Getenv("FROM_NUMBER")
-
-	tc = twilio.NewClient(twilioAccount, twilioKey, nil)
+	"github.com/elimisteve/do_reminder/twilhelp"
 )
 
 func main() {
@@ -29,21 +20,7 @@ func main() {
 	toPhone := os.Args[1]
 	msg := os.Args[2]
 
-	if len(toPhone) == 10 {
-		toPhone = "+1" + toPhone
-	}
-
-	if err := smsSend(toPhone, msg); err != nil {
+	if err := twilhelp.SendSMS(toPhone, msg); err != nil {
 		log.Fatalf("Error sending text to %s: %v\n", toPhone, err)
 	}
-}
-
-func smsSend(recipient, replyBody string) error {
-	params := twilio.MessageParams{Body: replyBody}
-	_, _, err := tc.Messages.Send(myNumber, recipient, params)
-	return err
-}
-
-func twilioResponse(s string) string {
-	return xml.Header + "<Response>\n" + s + "\n</Response>"
 }
