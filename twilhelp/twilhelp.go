7a1ebb9ede80779f40f4d7d9e42fd7e18b1b9747
Steve Phillips <steve@tryingtobeawesome.com>
2016-05-28 01:41:36 -0700

    Created twilhelp/SendSMS

---
+++ twilhelp/twilhelp.go
@@ -0,0 +1,40 @@
+// Steven Phillips / elimisteve
+// 2016.05.28
+
+package twilhelp
+
+import (
+	"log"
+	"os"
+
+	"github.com/subosito/twilio"
+)
+
+var (
+	twilioAccount = os.Getenv("TWILIO_ACCOUNT")
+	twilioKey     = os.Getenv("TWILIO_KEY")
+	fromNumber    = os.Getenv("FROM_NUMBER")
+
+	tc = twilio.NewClient(twilioAccount, twilioKey, nil)
+)
+
+func init() {
+	if twilioAccount == "" {
+		log.Fatalln("TWILIO_ACCOUNT not set")
+	}
+	if twilioKey == "" {
+		log.Fatalln("TWILIO_KEY not set")
+	}
+	if fromNumber == "" {
+		log.Fatalln("FROM_NUMBER not set")
+	}
+}
+
+func SendSMS(toNumber, replyBody string) error {
+	if len(toNumber) == 10 {
+		toNumber = "+1" + toNumber
+	}
+	params := twilio.MessageParams{Body: replyBody}
+	_, _, err := tc.Messages.Send(fromNumber, toNumber, params)
+	return err
+}
