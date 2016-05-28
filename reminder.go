23e8b89a13341ba71438f023f61c0f8ff04812b7
Steve Phillips <steve@tryingtobeawesome.com>
2016-05-28 00:21:16 -0700

    Generating random durations based on a negative one no longer panics

--- reminder.go
+++ reminder.go
@@ -247,6 +247,10 @@ func randDur(d time.Duration) time.Duration {
 	if d == 0 {
 		return d
 	}
+	if d < 0 {
+		d *= -1
+	}
+
 	randSecs := rand.Intn(int(d.Seconds()))
 	if rand.Intn(2) == 0 { // 50% chance
 		randSecs *= -1
