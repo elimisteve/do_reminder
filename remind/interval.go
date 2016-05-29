55d39d5ddf65563e32b947c6da35759c0739e867
Steve Phillips <steve@tryingtobeawesome.com>
2016-05-28 23:21:36 -0700

    Defined remind.IntervalSMS; creates Reminders in random order spaced within time range
    Included texts

---
+++ remind/interval.go
@@ -0,0 +1,72 @@
+// Steven Phillips / elimisteve
+// 2016.05.28
+
+package remind
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/elimisteve/do_reminder/twilhelp"
+)
+
+func init() {
+	rand.Seed(twilhelp.Now().Unix())
+}
+
+// IntervalSMS sends to toNumber each message from msgs throughout the
+// day, from start to finish, where this interval is broken into
+// len(msgs) pieces.
+func IntervalSMS(toNumber string, msgs []string, start, finish time.Time) ([]*Reminder, error) {
+	if len(toNumber) < 10 {
+		return nil, fmt.Errorf("Phone number '%s' is too short", toNumber)
+	}
+	if !start.Before(finish) {
+		return nil, fmt.Errorf("start time %s must be before finish %s", start, finish)
+	}
+	if len(msgs) == 0 {
+		return nil, errors.New("No messages included")
+	}
+
+	// E.g., 12pm to 5pm. If len(msgs) == 4, oneChunk == 1.25h
+	oneChunk := finish.Sub(start) / time.Duration(len(msgs))
+
+	reminders := make([]*Reminder, len(msgs))
+
+	nMsgs := len(msgs)
+	for i := 0; i < nMsgs; i++ {
+		// Randomly choose message
+		ndx := rand.Intn(len(msgs))
+		msg := msgs[ndx]
+
+		// Remove message so it can't be reused
+		msgs = append(msgs[:ndx], msgs[ndx+1:]...)
+
+		// Start + jump to beginning of correct interval + add random
+		// duration between 0 and oneChuck
+		nextRun := start.
+			Add(time.Duration(i) * oneChunk).
+			Add(randPositiveDur(oneChunk))
+
+		rem := Reminder{
+			Recipient:   toNumber,
+			Description: msg,
+			NextRun:     nextRun,
+			Created:     twilhelp.Now(),
+		}
+
+		reminders[i] = &rem
+	}
+
+	return reminders, nil
+}
+
+func randPositiveDur(d time.Duration) time.Duration {
+	randDur := RandDuration(d)
+	if randDur < 0 {
+		randDur = -randDur
+	}
+	return randDur
+}
---
+++ remind/interval_test.go
@@ -0,0 +1,70 @@
+// Steven Phillips / elimisteve
+// 2016.05.28
+
+package remind
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/elimisteve/do_reminder/twilhelp"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntervalSMS(t *testing.T) {
+	now := twilhelp.Now()
+
+	tests := []struct {
+		start, finish time.Time
+		msgs          []string
+	}{
+		{now, now.Add(3 * time.Minute), []string{
+			"Message 0", "Message 1", "Message 2",
+		}},
+	}
+
+	for _, test := range tests {
+		rems, err := IntervalSMS("0123456789", test.msgs, test.start, test.finish)
+		if err != nil {
+			t.Errorf("Error from IntervalSMS: %v", err)
+			continue
+		}
+
+		// Every reminder should be scheduled within window (between
+		// start and finish)
+
+		for i, rem := range rems {
+			t.Logf("rem[%d] == %#v\n", i, rem)
+
+			assert.WithinDuration(t,
+				test.start,
+				rem.NextRun,
+				test.finish.Sub(test.start),
+				fmt.Sprintf(
+					"Reminder scheduled for %s, should be between %s and %s",
+					rem.NextRun, test.start, test.finish))
+		}
+
+		oneChunk := test.finish.Sub(test.start) / time.Duration(len(test.msgs))
+
+		t.Logf("oneChunk == %v", oneChunk)
+
+		// Each pair of messages should be at most oneChunk*2 apart
+
+		for i := 0; i < len(rems)-1; i++ {
+			// Adjacent reminders _should_ be closer than oneChunk * 2
+			assert.WithinDuration(
+				t,
+				rems[i].NextRun,
+				rems[i+1].NextRun,
+				oneChunk*time.Duration(2),
+				fmt.Sprintf("Reminders %v and %v scheduled too far apart (%v)",
+					rems[i],
+					rems[i+1],
+					time.Duration(2)*rems[i+1].NextRun.Sub(rems[i].NextRun)),
+			)
+		}
+	}
+
+}
